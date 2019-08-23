a0c477b7d4eb056d38ca2b7de15921388e5fc645
Fedor <tikey,gm@gmail.com>
2019-08-23 15:11:07 +0300

    Texts translate and keyboards fix

---
+++ Data/Repository/UserReposiroty.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using ValeoBot.Data.Entities;
+
+namespace ValeoBot.Data.Repository
+{
+    public class UserReposiroty : IDataRepository<ValeoUser>
+    {
+        private readonly ILogger<UserReposiroty> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public UserReposiroty(
+            ApplicationDbContext context,
+            ILogger<UserReposiroty> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IEnumerable<ValeoUser> All()
+        { 
+            try
+            {
+                return _context.Users.ToList(); 
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error on get All Users.");
+                throw;
+            }
+        } 
+        public ValeoUser Get(long id)
+        {
+            try
+            {
+                return _context.Users
+                    .FirstOrDefault(e => e.Id == id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error on Get User: {id}.");
+                throw;
+            }
+        }
+
+        public ValeoUser Add(ValeoUser entity)
+        {
+            try
+            {
+                var value = Get(entity.Id);
+                if (value == null)
+                {
+                    var result = _context.Users.Add(entity);
+                    _context.SaveChanges();
+                    return result.Entity;
+                }
+                else
+                {
+                    var result = Get(entity.Id);
+                    return result;
+                }
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error on Add new User: {entity.ToString()}.");
+                throw;
+            }
+        }
+
+        public void Update(ValeoUser entity)
+        {
+            try
+            {
+                _context.Users.Update(entity);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error on Update User: {entity.ToString()}.");
+                throw;
+            }
+        }
+
+        public void Delete(ValeoUser entity)
+        {
+            try
+            {
+                _context.Users.Remove(entity);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error on Delete User: {entity.ToString()}.");
+                throw;
+            }
+        }
+        public ValeoUser[] Find(Func<ValeoUser, bool> predicator)
+        {
+            try
+            {
+                return _context.Users.Where(predicator).ToArray();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error on Find User: {predicator.ToString()}.");
+                throw;
+            }
+        }
+    }
+}
